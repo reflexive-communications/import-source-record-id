c9d7bb53c0c701b8e0e141bf1d22e3720e5946af
Sandor Semsey <sandor@es-progress.hu>
2022-03-19 16:33:49 +0100

    add copyright notice

--- CRM/ImportSourceRecordId/Activity/SourceRecordId/Controller.php
+++ CRM/ImportSourceRecordId/Activity/SourceRecordId/Controller.php
@@ -1,8 +1,19 @@
 <?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
 
 /**
  * State machine controller class.
  * Based on the legacy solution: https://github.com/civicrm/civicrm-core/blob/master/CRM/Activity/Import/Controller.php
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
  */
 class CRM_ImportSourceRecordId_Activity_SourceRecordId_Controller extends CRM_Core_Controller {
 
--- CRM/ImportSourceRecordId/Activity/SourceRecordId/Parser.php
+++ CRM/ImportSourceRecordId/Activity/SourceRecordId/Parser.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC https://civicrm.org/licensing
+ */
+
 
 /**
  * Class to parse activity csv files.
